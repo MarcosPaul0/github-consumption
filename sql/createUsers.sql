cc9c15ff3b273c5e040757add6cd4c71c708bc3e
MarcosPaulo <marcosphip7@gmail.com>
2022-06-26 12:39:32 -0300

    feat: usuários

--- sql/createUsers.sql
+++ sql/createUsers.sql
@@ -1,3 +1,4 @@
+-- GRUPO DE USUÁRIOS
 -- grupo que gerencia o banco de dados
 CREATE GROUP adm_group;
 
@@ -7,8 +8,16 @@ CREATE GROUP fill_group;
 -- grupo que somente visualiza os dados do banco de dados
 CREATE GROUP dev_group;
 
+-- PERMISSÕES
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO adm_group;
 
 GRANT SELECT, INSERT ON ALL TABLES IN SCHEMA public TO fill_group;
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO dev_group;
+
+-- USUÁRIOS DO BANCO DE DADOS
+CREATE USER adm_user IN adm_group;
+
+CREATE USER fill_user IN fill_group;
+
+CREATE USER dev_user IN dev_group;
